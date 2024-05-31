09f6c5c462efc079c84e9c55c2c2d22c7d06467a
Anas <muhammadanasbinadam@gmail.com>
2024-05-31 22:46:06 +0800

    Initial setup with tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameLayoutComponent } from './game-layout/game-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameLayoutComponent
   ],
   imports: [
     BrowserModule,
